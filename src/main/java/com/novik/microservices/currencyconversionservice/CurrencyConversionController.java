1fb939032955957cdf537cf380c034669dd7faed
Kate <ekat.novik@gmail.com>
2020-08-24 03:42:58 +0300

    Add using RestTemplate for getting data from currency-exchange microservice

--- src/main/java/com/novik/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/novik/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,9 +1,12 @@
 package com.novik.microservices.currencyconversionservice;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * CurrencyConversionController.
@@ -17,7 +20,15 @@ public class CurrencyConversionController {
       @PathVariable String to,
       @PathVariable BigDecimal quantity) {
 
-    return new CurrencyConversionBean(1L, from, to, BigDecimal.ONE, quantity, quantity, 0);
+    Map<String, String> uriVariables = new HashMap<>();
+    uriVariables.put("from", from);
+    uriVariables.put("to", to);
+    CurrencyConversionBean response = new RestTemplate().getForObject(
+        "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+        CurrencyConversionBean.class, uriVariables);
+
+    return new CurrencyConversionBean(response.getId(), from, to,
+        response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), 0);
   }
 
 }
